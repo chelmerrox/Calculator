eed4827ea05616057d123b0f2ccf976f0313990d
Losalini <C.Rokocakau@gmail.com>
2020-01-07 01:47:32 +1300

    Set all variable declarations in the global scope

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+//Variable declarations
+let i = 0;
+let j = 0;
+/*let k = 0;
+let l = 0;
+let m = 0; */
+
+let x_1;
+let x;
+
+let operationResult;
+let evaluation;
+
+let operator;
+let operators = [];
+let numbers = [];
+let value;
+let result = "";
