d4f6787b365de9c543f73e14b25bd57df4dc3b99
Losalini <C.Rokocakau@gmail.com>
2020-01-07 02:03:12 +1300

    Add references for numeric buttons, operator buttons, display text content, dark mode button and display screen

--- scripts/main.js
+++ scripts/main.js
@@ -16,3 +16,34 @@ let operators = [];
 let numbers = [];
 let value;
 let result = "";
+
+//numeric buttons
+const zero = document.getElementById('zero');
+const one = document.getElementById('one');
+const two = document.getElementById('two');
+const three = document.getElementById('three');
+const four = document.getElementById('four');
+const five = document.getElementById('five');
+const six = document.getElementById('six');
+const seven = document.getElementById('seven');
+const eight = document.getElementById('eight');
+const nine = document.getElementById('nine');
+
+//operator buttons
+const equal = document.getElementById('equal-to');
+
+const add = document.getElementById('add');
+const subtract = document.getElementById('subtract');
+const multiply = document.getElementById('multiply');
+const divide = document.getElementById('divide');
+const decimalPoint = document.getElementById('decimal-point');
+const negate = document.getElementById('negate-sign');
+const clear = document.getElementById('clear');
+const clearEntry = document.getElementById('clear-entry');
+const backspace = document.getElementById('backspace');
+
+const display = document.querySelector('p');
+
+const darkLightMode = document.getElementById('dark-light-mode');
+const thirdContainer = document.getElementById('third-container');
+const displayScreen = document.getElementById('display-screen');
